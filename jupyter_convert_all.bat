cae409134e08a61820f9e2743f8e755bb9cc0dc8
Max Mönch <max.moench@hexfourty.com>
2019-07-06 23:59:02 +0200

    Added UE09

---
+++ jupyter_convert_all.bat
@@ -0,0 +1,3 @@
+Call C:\ProgramData\Anaconda3\Scripts\activate.bat
+Call conda activate "python-beginner"
+jupyter nbconvert --to pdf --execute *.ipynb
---
+++ referent/jupyter_convert_all.bat
@@ -0,0 +1,4 @@
+Call C:\ProgramData\Anaconda3\Scripts\activate.bat
+Call conda activate "python-beginner"
+jupyter nbconvert --to html --execute *.ipynb
+PAUSE
