d41d80384301371b436f99b7f78ba5a838227591
ZZ DO Mönch <douk11ibag@fauad.fau.de>
2019-07-08 23:12:59 +0200

    Changed Path in install_conda

--- scripts/install_conda.bat
+++ scripts/install_conda.bat
@@ -1,5 +1,5 @@
 CALL W:
 CD W:\Desktop\pandas-basic\conda
 CALL C:\ProgramData\Anaconda3\Scripts\activate.bat
-CALL conda env create -f enviroment_python_beginner.yml
+CALL conda env create -f ..\conda\enviroment_python_beginner.yml 
 PAUSE
